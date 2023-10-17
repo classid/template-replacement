68dfbad966add70daf61628971eea2381aa209e8
Iqbal Atma Muliawan <iqbalatma@gmail.com>
2023-10-17 15:46:40 +0700

    add missing required parameter exception

--- src/Exceptions/MissingRequiredParameterException.php
+++ src/Exceptions/MissingRequiredParameterException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class MissingRequiredParameterException extends Exception
 {
-    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $message = "Missing required parameter", int $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
